532b258bf9763558b1241644c664fc234a0fbf8b
snakile <snakile@gmail.com>
2012-08-30 20:30:37 +0300

    added a test to movie_tests: testing a movie creation by a partial title

--- src/tests/movie_tests.py
+++ src/tests/movie_tests.py
@@ -17,10 +17,15 @@ class Test(unittest.TestCase):
         self.assertEqual(str(Movie('Pulp Fiction', 1995)), 'Pulp Fiction (1995)')
         self.assertEqual(str(Movie('Pulp Fiction', '1995')), 'Pulp Fiction (1995)')
     
-    def test_create_movie_from_query_simple(self):
+    def test_create_movie_from_query_exact_title(self):
         the_prestige = Movie.create_movie_from_query('the prestige')
         self.assertEqual(the_prestige.title, "The Prestige")
         self.assertEqual(the_prestige.year, 2006)
+    
+    def test_create_movie_from_query_partial_title(self):
+        the_matrix = Movie.create_movie_from_query('matrix')
+        self.assertEqual(the_matrix.title, "The Matrix")
+        self.assertEqual(the_matrix.year, 1999)
 
 
 if __name__ == "__main__":
