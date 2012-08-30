b1ca82f2babbe635f698f0bd9ecdc4eecf4ab6b9
snakile <snakile@gmail.com>
2012-08-30 21:40:36 +0300

    added an initial in-memory-db of tokens to exclude

---
+++ src/tokens_to_exclude/tokens_to_exclude_after_splitting.py
@@ -0,0 +1,30 @@
+classes = ['dvdrip',
+           'xvid',
+           'ac3',
+           'bdrip',
+           'brrip',
+           'hq',
+           'dxva'
+           ]
+
+rips = [
+        'dvdrip',
+        'bdrip',
+        'brrip',
+        'hdrip',
+        'hddvdrip',
+        'bluray',
+        'hddvd',
+        'dvdr',
+        'bd9',
+        'dvdr9',
+        'telesync',
+        'Telecine'
+        ]
+
+numbers = ['x264',
+           '480p',
+           '1080p',
+           '720p']
+
+tokens = classes + rips + numbers
---
+++ src/tokens_to_exclude/tokens_to_exclude_before_splitting.py
@@ -0,0 +1,29 @@
+long_tokens = ['XviD-ARROW',
+               'AC3-LEGi0N',
+               'AC3-LUSi',
+               'dxva-TiMPE',
+               'AC3-FLAWL3SS',
+               'AC3-ViSiON',
+               'XviD-ZMG',
+               'XviD-Noir',
+               'RoSubbed-playOFF',
+               'D-Z0N3',
+               'BDRip',
+               'WEB-DL',
+               'WEB.DL'
+               ]
+
+short_tokens = ['DVDRiP',
+          'XviD',
+          'AC3',
+          'LEGi0N',
+          'LUSi',
+          'TiMPE',
+          'dxva',
+          'FLAWL3SS',
+          'ViSiON',
+          'RERiP',
+          'XviD-ZMG'
+          ]
+
+tokens = long_tokens + short_tokens
