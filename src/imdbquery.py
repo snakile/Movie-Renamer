a7c810ca26b568e75d679f8161ce8102919cfa74
snakile <snakile@gmail.com>
2012-08-31 00:04:00 +0300

    ImdbQuery: added remove_non_alphanumeric_chars()

--- src/imdbquery.py
+++ src/imdbquery.py
@@ -1,8 +1,23 @@
+import re
+
 class ImdbQuery(str):
-    def exclude_terms(self, tems_to_exclude):
+    def exclude_terms(self, terms_to_exclude):
         r = self
-        for term in tems_to_exclude: r = r.replace(term, ' ')
+        for term in terms_to_exclude: r = r.replace(term, ' ')
         return ImdbQuery(r.lstrip().rstrip())
+    
+    def remove_non_alaphnumeric_chars(self):
+        terms = re.compile(r'[^a-zA-Z0-9]+').split(self)
+        r = ''
+        for t in terms:  
+            if t != '': r += ' ' + t
+        return ImdbQuery(r).lstrip()
     
     def lower(self):
         return ImdbQuery(super(ImdbQuery, self).lower())
+    
+    def lstrip(self):
+        return ImdbQuery(super(ImdbQuery, self).lstrip())
+    
+    def rstrip(self):
+        return ImdbQuery(super(ImdbQuery, self).rstrip())
--- src/tests/imdbquery_tests.py
+++ src/tests/imdbquery_tests.py
@@ -4,10 +4,15 @@ from imdbquery import ImdbQuery
 class TestImdbQuery(unittest.TestCase):
 
     def test_exclude_terms(self):
-        query = ImdbQuery('Inception.DVDRiP.XviD-ARROW')
+        filename = ImdbQuery('Inception.DVDRiP.XviD-ARROW')
         terms_to_exclude = ('dvdrip', 'xvid', 'arrow')
-        self.assertEqual(query.lower().exclude_terms(terms_to_exclude), 'inception. . -')
+        query = filename.lower().exclude_terms(terms_to_exclude)
+        self.assertEqual(query, 'inception. . -')
 
+    def test_remove_non_alphanumeric_chars(self):
+        filename = ImdbQuery('Inception.2010.DVDRip.x264.dxva-TiMPE')
+        query = filename.remove_non_alaphnumeric_chars()
+        self.assertEqual(query, 'Inception 2010 DVDRip x264 dxva TiMPE')
 
 if __name__ == "__main__":
     unittest.main()
