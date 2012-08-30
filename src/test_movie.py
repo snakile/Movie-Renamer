11a100197a209d02fadfade4113ccc197de04f5e
snakile <snakile@gmail.com>
2012-08-30 20:54:01 +0300

    deleted the old movie print-based-tests

--- src/test_movie.py
+++
@@ -1,7 +0,0 @@
-from movie import Movie
-print Movie('pulp fiction', 1995)
-print Movie('pulp fiction', '1995')
-print Movie.create_movie_from_query('the prestige')
-print Movie.create_movie_from_query('matrix')
-print Movie.create_movie_from_query('TheDarkKnight')
-print Movie.create_movie_from_query('lord.of.rings-two.towers')
