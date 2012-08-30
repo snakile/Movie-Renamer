2b51153afd2ce4267bf0176647752fc96237937f
snakile <snakile@gmail.com>
2012-08-30 20:27:46 +0300

    added a test to movie_tests: testing a simple movie generation from an imdb query

--- src/tests/movie_tests.py
+++ src/tests/movie_tests.py
@@ -16,6 +16,11 @@ class Test(unittest.TestCase):
     def test_str(self):
         self.assertEqual(str(Movie('Pulp Fiction', 1995)), 'Pulp Fiction (1995)')
         self.assertEqual(str(Movie('Pulp Fiction', '1995')), 'Pulp Fiction (1995)')
+    
+    def test_create_movie_from_query_simple(self):
+        the_prestige = Movie.create_movie_from_query('the prestige')
+        self.assertEqual(the_prestige.title, "The Prestige")
+        self.assertEqual(the_prestige.year, 2006)
 
 
 if __name__ == "__main__":
