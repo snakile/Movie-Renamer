1b243416703e08dda852bf3e9c7c47298ea4ddc5
snakile <snakile@gmail.com>
2012-09-02 21:24:23 +0300

    added a single test to the not yet created movie_renamer module

--- src/tests/movie_renamer_tests.py
+++ src/tests/movie_renamer_tests.py
@@ -4,8 +4,10 @@ import unittest
 class Test(unittest.TestCase):
 
 
-    def tes_rename(self):
-        pass
+    def test_rename(self):
+        old_filename = 'Inception.DVDRiP.XviD-ARROW.avi'
+        new_filename = 'Inception (2010).avi'
+        self.assertEqual(rename(old_filename), new_filename)
 
 
 if __name__ == "__main__":
