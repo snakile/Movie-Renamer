59683960da0b8ec7087c28d7e95d044d8ac80e9b
snakile <snakile@gmail.com>
2012-09-02 22:06:06 +0300

    added more movie_renamer tests to movie_renamer_test_data

--- src/tests/movie_renamer_test_data.py
+++ src/tests/movie_renamer_test_data.py
@@ -2,4 +2,16 @@ filenames = \
 {'Back.To.The.Future.Part.II.1989.1080p.BluRay.X264-LCHD.mkv': \
  'Back to the Future Part II (1989).mkv',
  'Inception.DVDRiP.XviD-ARROW.avi':
- 'Inception (2010).avi'}
+ 'Inception (2010).avi',
+ 'Back.To.The.Future.Part.II.1989.DVDRip.DivX.AC3.iNTERNAL-FFM.avi':
+ 'Back to the Future Part II (1989).avi',
+ 'Kill Bill.2003.DVDRip.XviD-DiCE.avi':
+ 'Kill Bill: Vol. 1 (2003).avi',
+ 'Gladiator[Extended.Edition]DvDrip.AC3[Eng]-aXXo.avi':
+ 'Gladiator (2000).avi',
+ 'Pirates_of_the_Caribbean_The_Curse_of_the_Black_Pearl.720p.ESiR.mkv':
+ 'Pirates of the Caribbean: The Curse of the Black Pearl (2003).mkv',
+ 'Shutter.Island.2010.480p.BRRip.x264.DXVA.AC3-FLAWL3SS.mkv':
+ 'Shutter Island (2010).mkv',
+ 'Monty.Python.and.the.Holy.Grail.DVDivX-SCHIZO.avi':
+ 'Monty Python and the Holy Grail (1975).avi'}
